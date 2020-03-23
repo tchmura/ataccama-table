7a44759cc04a7d3c5d357b42edb2cdabf4d2a175
Tomas Chmura <tomas.chmura1@gmail.com>
2020-03-23 19:34:12 +0100

    Updates

---
+++ precache-manifest.e9ab40702571c4f71a844e13e3a80404.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35b5d16f717c4ed8d7a99cf47012853b",
+    "url": "/ataccama-table/index.html"
+  },
+  {
+    "revision": "b6152edf037a56f09816",
+    "url": "/ataccama-table/static/js/2.32ff90e6.chunk.js"
+  },
+  {
+    "revision": "5839521b381d6ae8d4739a6544c25d5a",
+    "url": "/ataccama-table/static/js/2.32ff90e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a2ae1fc695f91524c9f",
+    "url": "/ataccama-table/static/js/main.1ae9c3ae.chunk.js"
+  },
+  {
+    "revision": "74e6ad5834e5cf0d86de",
+    "url": "/ataccama-table/static/js/runtime-main.593b7e2c.js"
+  }
+]);
